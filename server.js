b67fa962cfb4f5cb2353477d047430592921f4a6
Biwbong <bewrockgame1@gmail.com>
2025-07-21 23:19:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,14 @@ const app = express();
 
 // ใช้ middleware
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow non-browser tools (postman, curl)
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
