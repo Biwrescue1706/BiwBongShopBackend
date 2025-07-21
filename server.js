b02296e03fb880e7c7d47ba5b75ae6a14e8543d7
Phuwanat_Biw <bewrockgame1@gmail.com>
2025-07-21 15:32:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -33,20 +33,26 @@ const returnsRoutes = require('./routes/returns.routes');
 
 const app = express();
 
-app.use(cors({
-  origin: function(origin, callback) {
-    const allowedOrigins = [
-      'http://127.0.0.1:5500',
-      'https://biwbong-frontend.onrender.com'
-    ];
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('CORS policy...'), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'https://biwbong-frontend.onrender.com'
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+
+  // Handle OPTIONS method for preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
