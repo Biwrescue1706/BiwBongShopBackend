33e0fa8219fa540173d96769dd55e529fc8d1421
BiwBong1706 <bewrockgame1@gmail.com>
2025-08-11 14:29:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ if (!JWT_SECRET) {
 const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
-  'http://localhost:3000',
-  'https://biwbong-frontend.onrender.com',
-  'https://biwrescue1706.github.io',
-  'https://biwbongbackend.onrender.com'
+  'http://localhost:3000',    'https://biwbongshop.onrender.com', 'https://biwbongbackend.onrender.com'
 ];
 
 // สร้าง app
